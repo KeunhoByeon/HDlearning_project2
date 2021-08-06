0cb0caa07e50c39a295bc8f39bd18975b0c4052e
변근호 <bkh5922@kookmin.kr>
2021-08-07 05:54:16 +0900

    add linear encoder

--- onlinehd/encoder.py
+++ onlinehd/encoder.py
@@ -1,6 +1,9 @@
 import math
 
+import numpy as np
 import torch
+from tqdm import tqdm
+
 
 class Encoder(object):
     '''
@@ -22,13 +25,14 @@ class Encoder(object):
 
         dim (int, > 0): Target dimension for output data.
     '''
-    def __init__(self, features : int, dim : int = 4000):
+
+    def __init__(self, features: int, dim: int = 4000):
         self.dim = dim
         self.features = features
         self.basis = torch.randn(self.dim, self.features)
-        self.base = torch.empty(self.dim).uniform_(0.0, 2*math.pi)
+        self.base = torch.empty(self.dim).uniform_(0.0, 2 * math.pi)
 
-    def __call__(self, x : torch.Tensor):
+    def __call__(self, x: torch.Tensor):
         '''
         Encodes each data point in `x` to high dimensional space.
         The encoded representation of the `(n?, features)` samples described
@@ -52,15 +56,15 @@ class Encoder(object):
         '''
 
         n = x.size(0)
-        bsize = math.ceil(0.01*n)
+        bsize = math.ceil(0.01 * n)
         h = torch.empty(n, self.dim, device=x.device, dtype=x.dtype)
         temp = torch.empty(bsize, self.dim, device=x.device, dtype=x.dtype)
 
         # we need batches to remove memory usage
         for i in range(0, n, bsize):
-            torch.matmul(x[i:i+bsize], self.basis.T, out=temp)
-            torch.add(temp, self.base, out=h[i:i+bsize])
-            h[i:i+bsize].cos_().mul_(temp.sin_())
+            torch.matmul(x[i:i + bsize], self.basis.T, out=temp)
+            torch.add(temp, self.base, out=h[i:i + bsize])
+            h[i:i + bsize].cos_().mul_(temp.sin_())
         return h
 
     def to(self, *args):
@@ -80,3 +84,64 @@ class Encoder(object):
         self.basis = self.basis.to(*args)
         self.base = self.base.to(*args)
         return self
+
+
+class LinearEncoder(object):
+    def __init__(self, features: int, dim: int = 4000, m=1000):
+        self.dim = dim
+        self.features = features
+        self.m = m
+        self.basis = torch.randint(0, 2, size=(self.dim, self.features)) * 2 - 1
+        self.level = np.random.randint(2, size=self.dim) * 2 - 1
+
+        self.levels = []
+        temp = np.ones(self.dim)
+        for i in range(1, self.m + 1):
+            # flip = int(self.dim / i)
+            flip = int(self.dim / m) * i
+            temp[:flip] = -1
+            temp[flip:] = 1
+            self.levels.append(np.array(self.level * temp))
+        self.levels = torch.from_numpy(np.array(self.levels))
+
+    def __call__(self, x: torch.Tensor):
+        n = x.size(0)
+        bsize = math.ceil(0.05 * n)
+        h = torch.empty(n, self.dim, device=x.device, dtype=x.dtype)
+
+        # we need batches to remove memory usage
+        temp = torch.empty(bsize, self.features, self.dim)
+        for i in tqdm(range(0, n, bsize), leave=False):
+            try:
+                l = ((x[i:i + bsize].cpu().detach().numpy() + 1) / 2 * (self.m - 1)).astype(int)
+                temp = self.levels[l] * self.basis.T
+                temp = temp.view(-1, self.features, self.dim)
+                h[i:i + bsize] = torch.sum(temp, dim=1)
+            except Exception as e:
+                print(i, '/', n)
+                print('l', l.shape)
+                print('self.levels', self.levels.shape)
+                print('self.basis.T', self.basis.T.shape)
+                print(temp.shape)
+                print(self.levels[l] * self.basis.T)
+                raise e
+
+        return h
+
+    def to(self, *args):
+        self.basis = self.basis.to(*args)
+        self.levels = self.levels.to(*args)
+        return self
+
+
+if __name__ == '__main__':
+    from dataloader import load_isolet
+
+    e = LinearEncoder(617)
+    x, x_test, _, _ = load_isolet(data_dir='../data', normalize=False)
+    if torch.cuda.is_available():
+        x= x.cuda()
+        x_test = x_test.cuda()
+        e.to(x.device)
+    # print(e(x).size())
+    print(e(x_test).size())
